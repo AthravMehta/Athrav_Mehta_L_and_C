be82ab241f870d0acf0c12dc6f366e8b3fd7931b
Athrav Mehta <athrav.mehta@intimetec.com>
2025-04-08 19:02:14 +0530

    CRUD API Assignment Added

---
+++ CRUD_API/ProductManagement/ProductManagement/Constants/ErrorConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace ProductManagement.Constants
+{
+    public static class ErrorConstants
+    {
+        public const string IdCannotBeNull = "id cannot be null";
+        public const string IdCannotBeZero = "id cannot be zero";
+        public const string ProductNotFound = "Product not found";
+        public const string ProductsNotFound = "Products not found";
+        public const string ProductIsNull = "Product is null";
+        public const string ProductWithSimilarNameExists = "Product with similar name exists";
+        public const string UpdatedProductIsNull = "Updated Product is null";
+        public const string InvalidId = "Invalid Id";
+    }
+}
