2942a2cf4aa985a599313f9a40b32e00c68d04c8
Athrav Mehta <athrav.mehta@intimetec.com>
2025-04-28 19:54:42 +0530

    Boundries Assignment Added

---
+++ Boundries Assignment/CoordinatesFinder/Models/LocationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CoordinatesFinder.Models
+{
+    public class LocationModel
+    {
+        public string lat { get; set; }
+        public string lon { get; set; }
+        public string display_name { get; set; }
+    }
+    public record Coordinates(double Latitude, double Longitude);
+}
---
+++ Boundries Assignment/CoordinatesFinder/Program.cs
@@ -0,0 +1,61 @@
+﻿using CoordinatesFinder.Constants;
+using CoordinatesFinder.Exceptions;
+using CoordinatesFinder.Helpers;
+using CoordinatesFinder.Models;
+using Microsoft.Extensions.Configuration;
+
+
+namespace CoordinatesFinder
+{
+    public class CoordinatesFinder
+    {
+        public static async Task Main(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(AppConstants.AppSettingsJson).Build();
+
+            HttpClient client = new HttpClient();
+            CoordinatesFinderHelper coordinatesFinder = new CoordinatesFinderHelper(client, configuration);
+
+            try
+            {
+                var location = GetLocation();
+                Coordinates coordinates = await coordinatesFinder.GetCoordinatesFromLocationAsync(location);
+                Console.WriteLine($"Latitude: {coordinates.Latitude}, Longitude: {coordinates.Longitude}");
+            }
+            catch (GeocodingException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        public static string GetLocation()
+        {
+            string input;
+            do
+            {
+                Console.Write("Enter location to get Coordintates: ");
+                input = Console.ReadLine()?.Trim();
+
+            } while (!ValidateLocation(input));
+
+            return input;
+        }
+
+
+        public static bool ValidateLocation(string location)
+        {
+            if (string.IsNullOrWhiteSpace(location))
+            {
+                Console.WriteLine("Entered Input Address is Empty");
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
