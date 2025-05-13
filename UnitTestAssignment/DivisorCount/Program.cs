68a44e51b0759c1c965b28957bea87651bb6ea69
Athrav Mehta <athrav.mehta@intimetec.com>
2025-05-13 19:06:08 +0530

    Folder Structure Improved

--- UnitTestAssignment/DivisorCount/Program.cs
+++ UnitTestAssignment/DivisorCount/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Program
+﻿using DivisorCount;
+
+public class Program
 {
     public static void Main(string[] args)
     {
@@ -46,39 +48,5 @@
         {
             Console.WriteLine(answer);
         }
-    }
-}
-
-public class DivisorCountSolver
-{
-    public static int CountMatchingDivisorPairs(int number)
-    {
-        if (number < 0)
-        {
-            throw new Exception($"Input must be non-negative {number}.");
-        }
-
-        int matchingPairCount = 0;
-        for (int i = 1; i < number; i++)
-        {
-            if (GetDivisorCount(i) == GetDivisorCount(i + 1))
-            {
-                matchingPairCount++;
-            }
-        }
-        return matchingPairCount;
-    }
-
-    private static int GetDivisorCount(int number)
-    {
-        int divisorCount = 0;
-        for (int i = 1; i <= Math.Sqrt(number); i++)
-        {
-            if (number % i == 0)
-            {
-                divisorCount += (i == number / i) ? 1 : 2;
-            }
-        }
-        return divisorCount;
     }
 }
