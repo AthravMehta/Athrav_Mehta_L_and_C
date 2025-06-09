8c4b2d0e57d23619a5eab02774ee634877d13b9d
Athrav Mehta <athrav.mehta@intimetec.com>
2025-06-09 19:10:14 +0530

    Added Custom Exception Class

---
+++ FinalAssignment/NewsAggregation/Exceptions/ApiException.cs
@@ -0,0 +1,27 @@
+﻿using NewsAggregation.Utilities;
+
+namespace NewsAggregation.Exceptions
+{
+    public class ApiException : Exception
+    {
+        public ErrorResponse.ErrorEnum ErrorCode { get; set; }
+
+        public ApiException(string details, Exception ex = null, Serilog.ILogger logger = null) : base(details, ex)
+        {
+            if(logger != null)
+            {
+                logger.Error(Message);
+            }
+            ErrorCode = ErrorResponse.ErrorEnum.BadRequest;
+        }
+        
+        public ApiException(ErrorResponse.ErrorEnum errCode ,string details = null, Exception ex = null, Serilog.ILogger logger = null) : base(ErrorResponse.GetErrorMessage(errCode) + " : " + details, ex)
+        {
+            if(logger != null)
+            {
+                logger.Error(Message);
+            }
+            ErrorCode = ErrorResponse.ErrorEnum.BadRequest;
+        }
+    }
+}
