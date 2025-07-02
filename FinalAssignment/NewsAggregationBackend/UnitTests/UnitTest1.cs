5642066c302214bda8c3b38878df95f8c95fae01
Athrav Mehta <athrav.mehta@intimetec.com>
2025-07-02 18:16:04 +0530

    Change in Folder Structure to Set up Unit test

---
+++ FinalAssignment/NewsAggregationBackend/UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NewsAggregation.Entities;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Article 
+        }
+    }
+}
